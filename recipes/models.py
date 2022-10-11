94b2e7ac30674861168664a8c4f69cbe5e97b638
TaraHelberg <taralhelberg@outlook.com>
2022-10-11 06:59:50 +0000

    Add templates base & index.html

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class recipe(models.Model):
+    """
+    Model for the Post Recipe
+    Used & Modified from I think therefore I blog
+    Credit to Code Institute
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="recipes")
+    update_on = models.DateTimeField(auto_now=True)
+    create_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    description = models.TextField(blank=True, help_text="Describe The Recipe")
+    serves = models.IntegerField(validators=[
+                                      MinValueValidator(1),
+                                      MaxValueValidator(100)
+                                    ])
+    prep_time = models.IntegerField(validators=[
+                                      MinValueValidator(1),
+                                      MaxValueValidator(100)
+                                    ])
+    cooking_time = models.IntegerField(validators=[
+                                      MinValueValidator(1),
+                                      MaxValueValidator(100)
+                                    ])
+    ingredients = models.TextField()
+    method = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    status = models.ImageField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='recipe_likes',
+                                   blank=True)
+
+
+class Meta:
+    """
+    Orders the recipes in Descending order.
+    """
+    ordering = ['-created_on']
+
+
+def __str__(self):
+    """
+    Returns a string method "AKA :The Magic method""
+    """
+    return str(self.title)
+
+
+def amount_of_likes(self):
+    """
+    Shows the number of likes on a recipe.
+    """
+    return self.likes.count()
+
+
+class Comment(models.Model):
+    """
+    Add a comment.
+    Used from I think therefore I blog
+    Credit to Code Institute
+    """
+    recipe = models.ForeignKey(recipe, on_delete=models.CASCADE,
+                               related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        """
+        Orders the comments in Ascending order.
+        """
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
