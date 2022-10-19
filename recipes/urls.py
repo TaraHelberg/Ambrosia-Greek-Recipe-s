5e807427812f8c1ef681088ff99df5f24c0a95a8
TaraHelberg <taralhelberg@outlook.com>
2022-10-19 12:54:16 +0000

    Add View, Tempalte and Urls for Recipe details.html view entire recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('browse/', views.RecipeList.as_view(), name='browse'),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
 ]
