038d9eefc1052a9b522e6576a0c4bde9ba9b9283
TaraHelberg <taralhelberg@outlook.com>
2022-10-21 08:07:12 +0000

    Add ability to like and unlike recipes and add Fontawesome & stylsheet.css links to base.html

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('browse/', views.RecipeList.as_view(), name='browse'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+    path('like/<slug:slug>/', views.PostLike.as_view(), name='recipe_likes'),
 ]
